9aac3c541f221d780f27c41fbe101351fc8a1803
majahoberg <eaamhob@students.eaaa.dk>
2020-03-18 14:59:58 +0100

    14:59 18-03-2020

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,1 +1,16 @@
 "use strict";
+
+
+
+fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
+
+.then (function(data) {
+	return data.json();
+})
+
+
+.then(function(post) {
+    const tid = document.getElementById("tid");
+    tid.innerHTML = ("Klokken er: "+ post.datetime);
+
+    });

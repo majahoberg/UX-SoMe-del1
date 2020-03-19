8f096f323146c14b404853e6a219e3bb9c20ebbf
majahoberg <eaamhob@students.eaaa.dk>
2020-03-19 11:30:28 +0100

    11:30 19-03-2020

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -14,10 +14,10 @@ fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
     tid.innerHTML = (post.day_of_week);
 
 		if(post.day_of_week === 1) {
-			document.getElementById("tid").insertAdjecentHTML('beforeend', "<p>Monday</p>");
+			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
 
 		} else if (post.day_of_week === 2) {
-			document.getElementById("tid").insertAdjecentHTML('beforeend', "<p>Monday</p>");
+			document.getElementById("tid").InnerHT('beforeend', "<p>Monday</p>");
 
 		} else if (post.day_of_week === 3) {
 			document.getElementById("tid").insertAdjecentHTML('beforeend', "<p>Monday</p>");

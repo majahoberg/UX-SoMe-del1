e5c06119885d0bba0ec953c773e9c7e3020ad4ce
majahoberg <eaamhob@students.eaaa.dk>
2020-03-19 13:54:53 +0100

    13:54 19-03-2020

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -10,28 +10,34 @@ fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
 
 
 .then(function(post) {
-    const tid = document.getElementById("tid");
-    tid.insertAdjecentHTML = (post.day_of_week);
+    const tid = document.getElementById("dag");
+    dag.insertAdjecentHTML = (post.day_of_week);
+		const tid = document.getElementById("tid");
+	 	const datoOGtid = new Date(post.datetime);
+
+		tid.insertAdjacentHTML("beforeend", "<br>Tid: " + datoOGtid.getHours() + ":" + datoOGtid.getMinutes());
+
+	 	dag.insertAdjacentHTML("beforeend", "<br>Tid: " + datoOGtid.getHours() + ":" + datoOGtid.getMinutes());
 
 		if(post.day_of_week == 1) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Monday</p>");
-			
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Monday</p>");
+
 		} else if (post.day_of_week == 2) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Tuesday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Tuesday</p>");
 
 		} else if (post.day_of_week == 3) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Wednesday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Wednesday</p>");
 
 		} else if (post.day_of_week == 4) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Thursday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Thursday</p>");
 
 		}	else if (post.day_of_week == 5) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Friday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Friday</p>");
 
 		}	else if (post.day_of_week == 6) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Saturday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Saturday</p>");
 
 		}	else if (post.day_of_week == 7) {
-			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Sunday</p>");
+			document.getElementById("dag").insertAdjacentHTML('beforeend', "<p>Sunday</p>");
 				}
     });

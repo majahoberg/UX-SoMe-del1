9816ee184c99c1b2095029ba675b4d206f1d7051
majahoberg <eaamhob@students.eaaa.dk>
2020-03-19 12:37:18 +0100

    12:37 19-03-2020

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,27 +11,27 @@ fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
 
 .then(function(post) {
     const tid = document.getElementById("tid");
-    tid.innerHTML = (post.day_of_week);
+    tid.insertAdjecentHTML = (post.day_of_week);
 
-		if(post.day_of_week === 1) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		if(post.day_of_week == 1) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Monday</p>");
+			
+		} else if (post.day_of_week == 2) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Tuesday</p>");
 
-		} else if (post.day_of_week === 2) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		} else if (post.day_of_week == 3) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Wednesday</p>");
 
-		} else if (post.day_of_week === 3) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		} else if (post.day_of_week == 4) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Thursday</p>");
 
-		} else if (post.day_of_week === 4) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		}	else if (post.day_of_week == 5) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Friday</p>");
 
-		}	else if (post.day_of_week === 5) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		}	else if (post.day_of_week == 6) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Saturday</p>");
 
-		}	else if (post.day_of_week === 6) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
-
-		}	else if (post.day_of_week === 7) {
-			document.getElementById("tid").innerHTML('beforeend', "<p>Monday</p>");
+		}	else if (post.day_of_week == 7) {
+			document.getElementById("tid").insertAdjacentHTML('beforeend', "<p>Sunday</p>");
 				}
     });
